32f9185b948cb3c9342002970f50190a3ac3d527
Fabio Carrara <fabio.carrara@isti.cnr.it>
2020-11-09 11:15:07 +0100

    improved log generation with best reconstruction images

--- show.py
+++ show.py
@@ -4,13 +4,13 @@ import pandas as pd
 
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
-from matplotlib.ticker import FuncFormatter
+from matplotlib.ticker import FuncFormatter, AutoMinorLocator
 
 from tqdm import tqdm
 import expman
 
 import seaborn as sns
-sns.set_theme(style='whitegrid')
+sns.set_theme(style='darkgrid')
 
 # avoid importing mvtec_ad and load tf for mere plotting
 # from mvtec_ad import textures, objects
@@ -38,7 +38,7 @@ objects = [
 
 def plot_log(args):
 
-    formatter = lambda x, pos: f'{(x // 1000):g}k' if x >= 1000 else str(x)
+    formatter = lambda x, pos: f'{(x // 1000):g}k' if x >= 1000 else f'{x:g}'
     formatter = FuncFormatter(formatter)
 
     exps = expman.gather(args.run)
@@ -54,19 +54,46 @@ def plot_log(args):
             metric_log = exp.path_to(f'metrics_{category}.csv')
             metric_log = pd.read_csv(metric_log)
             
-            fig, axes = plt.subplots(2, 2)
-            ax1, ax2, ax3, ax4 = axes.flat
+            best_recon_file = exp.path_to(f'best_recon_{category}.png')
+            best_recon = plt.imread(best_recon_file)
             
+            # prepare figure
+            zoom = 0.7
+            fig = plt.figure(figsize=(20 * zoom, 8 * zoom))
+            # gridspec and axes for plots
+            gs = fig.add_gridspec(ncols=2, nrows=2, hspace=0.05, wspace=0.05, right=0.5)
+            ax1 = fig.add_subplot(gs[0,0])
+            ax2 = fig.add_subplot(gs[0,1])
+            ax3 = fig.add_subplot(gs[1,0], sharex=ax1)
+            ax4 = fig.add_subplot(gs[1,1], sharex=ax2)
+            # gridspec and axes for preview images
+            gs2 = fig.add_gridspec(ncols=1, nrows=2, hspace=0, wspace=0, left=0.55)
+            ax5 = fig.add_subplot(gs2[:,0])
+            # ticklabels format
             ax1.xaxis.set_major_formatter(formatter)
             ax2.xaxis.set_major_formatter(formatter)
             ax3.xaxis.set_major_formatter(formatter)
             ax4.xaxis.set_major_formatter(formatter)
+            ax5.axis('off')
+            # minor ticks position
+            ax3.xaxis.set_minor_locator(AutoMinorLocator())
+            ax4.xaxis.set_minor_locator(AutoMinorLocator())
+            # minor grid style
+            ax1.grid(b=True, which='minor', linewidth=0.5)
+            ax2.grid(b=True, which='minor', linewidth=0.5)
+            ax3.grid(b=True, which='minor', linewidth=0.5)
+            ax4.grid(b=True, which='minor', linewidth=0.5)
+            # right y-axes
+            ax2.yaxis.set_label_position("right")
+            ax2.yaxis.tick_right()
+            ax4.yaxis.set_label_position("right")
+            ax4.yaxis.tick_right()
             
             # generator losses
             gen = ['generator_encoder_loss', 'images_reconstruction_loss',
                    'latent_reconstruction_loss', 'generator_encoder_total_loss']
             train_log.plot(x='step', y=gen, logy='sym', ax=ax1)
-            ax1.legend(loc='lower left', bbox_to_anchor=(0.0, 1.2))
+            ax1.legend(loc='lower left', bbox_to_anchor=(0.0, 1.0))
            
             # discriminator losses
             dis = ['discriminator_loss', 'gradient_penalty_loss', 'discriminator_total_loss']
@@ -76,15 +103,19 @@ def plot_log(args):
             # scores
             scores = ['real_score', 'fake_score']            
             train_log.plot(x='step', y=scores, logy='sym', ax=ax2)
-            ax2.legend(loc='lower right', bbox_to_anchor=(1.0, 1.2))
+            ax2.legend(loc='lower right', bbox_to_anchor=(1.0, 1.0))
             
             # metrics
             metric_log.plot(x='step', y=['auc', 'balanced_accuracy'], ax=ax4)
             ax4.legend(loc='upper right', bbox_to_anchor=(1.0, -0.2))
             
+            # best reconstruction
+            ax5.imshow(best_recon)
+            ax5.margins(0)
+            
             # params
             params_str = exp.params.to_string()
-            plt.figtext(0.0, 0.5, params_str, ha='right', va='center', family='monospace')
+            plt.figtext(0.08, 0.5, params_str, ha='right', va='center', family='monospace')
             
             # save sigle figure in exp folder
             log_pdf = exp.path_to(f'log_{category}.pdf')
